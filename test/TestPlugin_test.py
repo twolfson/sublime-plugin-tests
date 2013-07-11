95034495fdd07d6299e6a45727a9d2679a451c2e
Todd Wolfson <todd@twolfson.com>
2013-07-11 01:42:46 -0700

    Recording thoughts...

--- test/TestPlugin_test.py
+++ test/TestPlugin_test.py
@@ -1,12 +1,18 @@
 import unittest
 import subprocess
 
+# TODO: Current vision...
+# nosetests loads a test runner file
+# could require users to have test hook command
+# but... why not define our own commands inside of test framework? o_o
+# which import ...
+# never mind, the plugin should be accessible inside of sublime text already
+# so we can do all of our run commands if necessary
+# TODO: we still need to think about how to handle prompts. probably override them somehow.
+# whether it is at the sublime level or class level (getattr, setattr)
+# this is necessary because I don't think we can force selection in a quick panel listing
+
 
 class BasicTest(unittest.TestCase):
     def test_one_is_one(self):
-        cmd = '''
-        with f as open('tmp.txt'):
-            f.write('hello')
-        '''
-        print cmd
-        subprocess.call(['sublime_text', '--command', 'exec' % cmd])
+        subprocess.call(['sublime_text', '--command', 'write_to_file'])
