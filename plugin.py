95122faed9ac544311d346d31691fbde8f9f84a0
Todd Wolfson <todd@twolfson.com>
2013-09-06 02:34:36 -0700

    Breaking out each action into a method

--- plugin.py
+++ plugin.py
@@ -6,6 +6,54 @@ import sublime_plugin
 __dir__ = path.dirname(path.abspath(__file__))
 Region = sublime.Region
 
+class ScratchView:
+    def __init__(self, window=None):
+        # Fallback window to active window
+        if not window:
+            window = sublime.active_window()
+
+        # Generate and save new view
+        self.view = window.new_file()
+
+    def clear_content(self):
+        """ Clear out view content """
+        # Localize view
+        view = self.view
+
+        # Generate an edit to clear out the view
+        edit = view.begin_edit()
+        view.erase(edit, Region(0, view.size()))
+        view.end_edit(edit)
+
+    def set_content(self, content):
+        """ Set the view content """
+        # Localize view
+        view = self.view
+
+        # Clear out content
+        self.clear_content()
+
+        # Set the content
+        edit = view.begin_edit()
+        view.insert(edit, 0, content)
+        view.end_edit(edit)
+
+    def clear_sel(self):
+        """ Clear out view selection """
+        self.view.sel().clear()
+
+    def set_sel(self, regions):
+        """ Set out view selection via RegionSet """
+        # Clear out selection
+        self.clear_sel()
+
+        # Add each region to selection RegionSet
+        # DEV: Attribution to sublime-invert-selection
+        sel = self.view.sel()
+        for region in regions:
+            sel.add(region)
+
+
 class TmpTestCommand(sublime_plugin.ApplicationCommand):
     @classmethod
     def split_sel(cls, input):
@@ -49,18 +97,12 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
         content = input_obj['content']
 
         # Generate new scratch file
-        view = sublime.active_window().new_file()
+        view = ScratchView()
 
         # Output single.input to scratch
-        edit = view.begin_edit()
-        view.insert(edit, 0, content)
-        view.end_edit(edit)
+        view.set_content(content)
 
         # Update selection
-        # DEV: Attribution to sublime-invert-selection
-        view.sel().clear()
-        for region in target_sel:
-            view.sel().add(region)
 
         # Run command
         view.run_command('left_delete')
@@ -83,6 +125,7 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
         actual_sel = view.sel()
         print expected_sel[0] == actual_sel[0]
 
+        # TODO: Move this into a finally of try/except block
         # Close the view
         # DEV: Empty the view to prevent a prompt on close
         edit = view.begin_edit()
