1572709dce144388285e0054500dfce2977e16b5
Todd Wolfson <todd@twolfson.com>
2013-09-06 00:25:59 -0700

    Successfully updating selection

--- plugin.py
+++ plugin.py
@@ -3,21 +3,30 @@ import sublime_plugin
 
 class TmpTestCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        # # Load in single.input
-        # with open('example/left_delete/test_files/single.input.py') as f:
-        #     content = f.read()
-        content = 'abc'
+        # Load in single.input
+        with open('example/left_delete/test_files/single.input.py') as f:
+            content = f.read()
 
-        # # Generate new scratch file
-        # scratch_view = sublime.active_window().new_file()
-        scratch_view = sublime.active_window().active_view()
+        # Generate new scratch file
+        # view = sublime.active_window().new_file()
+        view = sublime.active_window().active_view()
 
-        # Output single.input to scratch
-        insert_edit = scratch_view.begin_edit()
-        scratch_view.insert(insert_edit, 0, content)
-        scratch_view.end_edit(insert_edit)
+        # # Output single.input to scratch
+        # edit = view.begin_edit()
+        # view.insert(edit, 0, content)
+        # view.end_edit(edit)
 
-        # TODO: Update selection
+        # Update selection
+        target_sels = [sublime.Region(7, 7)]
+        # content = """
+        # def abc():
+        #     pass
+        # """
+        # DEV: Attribution to sublime-invert-selection
+        view.sel().clear()
+        for target_sel in target_sels:
+            view.sel().add(target_sel)
+
         # TODO: Run command
         # TODO: Load in single.output
         # TODO: Assert input to output
