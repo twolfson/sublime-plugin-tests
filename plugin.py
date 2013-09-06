fcedcdb58f15503b39ace0e3efaaa00e5caeea56
Todd Wolfson <todd@twolfson.com>
2013-09-06 00:13:55 -0700

    Successfully loading input

--- plugin.py
+++ plugin.py
@@ -2,4 +2,14 @@ import sublime_plugin
 
 class TmpTestCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        print 'a'
+        # TODO: Load in single.input
+        with open('example/left_delete/test_files/single.input.py') as f:
+            print f.read()
+        # TODO: Generate new scratch file
+        # TODO: Output single.input to scratch
+        # TODO: Update selection
+        # TODO: Run command
+        # TODO: Load in single.output
+        # TODO: Assert input to output
+        # TODO: Assert current selection to output selection
+        pass
