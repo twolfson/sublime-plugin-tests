c15f2a57b88ca9d5e3427dad5b8c7cd05361e7c2
Todd Wolfson <todd@twolfson.com>
2013-09-06 01:46:34 -0700

    Working on matching selection from flat files

--- plugin.py
+++ plugin.py
@@ -1,17 +1,37 @@
+import re
 import sublime
 import sublime_plugin
 
 class TmpTestCommand(sublime_plugin.ApplicationCommand):
+    @classmethod
+    def split_sel(cls, input):
+        # Find all indications for selection
+        # TODO: Robustify with larger selection and escaping
+        # TODO: We will prob need to iterate in reverse to handle clipping
+        print re.findall(r'\|', input)
+        # for match in re.finditer(r'\|', input):
+        #     print match.group(0)
+
+        # Return a selection and content
+        return {
+            'sel': [],
+            'content': input
+        }
+
     def run(self):
         # Load in single.input
         with open('example/left_delete/test_files/single.input.py') as f:
-            content = f.read()
+            input = f.read()
+
+        print self.__class__.split_sel(input)
 
         # Break up target selection from content
         target_sel = [sublime.Region(7, 7)]
         # content = """def abc():
 #     pass
 # """
+
+        return
 
         # Generate new scratch file
         # view = sublime.active_window().new_file()
@@ -41,13 +61,11 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
     pass
 """
 
-        # TODO: Assert input to output
+        # Assert input to output
         file_region = sublime.Region(0, view.size())
         actual_content = view.substr(file_region)
-        # print expected_content, actual_content
-        print expected_content == actual_content
+        assert expected_content == actual_content
 
-        # TODO: Assert current selection to output selection
+        # Assert current selection to output selection
         actual_sel = view.sel()
-        print expected_sel, actual_sel
-        print expected_sel[0] == actual_sel[0]
+        assert expected_sel[0] == actual_sel[0]
