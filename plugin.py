e5f64cd7d7eaa0cdabdf8996bfdea86837489dd9
Todd Wolfson <todd@twolfson.com>
2013-09-06 00:19:21 -0700

    Successfully entering content

--- plugin.py
+++ plugin.py
@@ -1,12 +1,22 @@
+import sublime
 import sublime_plugin
 
 class TmpTestCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        # TODO: Load in single.input
-        with open('example/left_delete/test_files/single.input.py') as f:
-            print f.read()
-        # TODO: Generate new scratch file
-        # TODO: Output single.input to scratch
+        # # Load in single.input
+        # with open('example/left_delete/test_files/single.input.py') as f:
+        #     content = f.read()
+        content = 'abc'
+
+        # # Generate new scratch file
+        # scratch_view = sublime.active_window().new_file()
+        scratch_view = sublime.active_window().active_view()
+
+        # Output single.input to scratch
+        insert_edit = scratch_view.begin_edit()
+        scratch_view.insert(insert_edit, 0, content)
+        scratch_view.end_edit(insert_edit)
+
         # TODO: Update selection
         # TODO: Run command
         # TODO: Load in single.output
