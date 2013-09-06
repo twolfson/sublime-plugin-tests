6f23670fa54e8d1be07c92c1d50c727405fe2a86
Todd Wolfson <todd@twolfson.com>
2013-09-06 02:03:43 -0700

    Successfully running full assertion

--- plugin.py
+++ plugin.py
@@ -43,49 +43,45 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
         with open(__dir__ + '/example/left_delete/test_files/single.input.py') as f:
             input = f.read()
 
-        print self.__class__.split_sel(input)
-
         # Break up target selection from content
-        target_sel = [sublime.Region(7, 7)]
-        # content = """def abc():
-#     pass
-# """
-
-        return
+        input_obj = self.__class__.split_sel(input)
+        target_sel = input_obj['sel']
+        content = input_obj['content']
 
         # Generate new scratch file
-        # view = sublime.active_window().new_file()
-        view = sublime.active_window().active_view()
+        view = sublime.active_window().new_file()
 
-        # # Output single.input to scratch
-        # edit = view.begin_edit()
-        # view.insert(edit, 0, content)
-        # view.end_edit(edit)
+        # Output single.input to scratch
+        edit = view.begin_edit()
+        view.insert(edit, 0, content)
+        view.end_edit(edit)
 
-        # # Update selection
-        # # DEV: Attribution to sublime-invert-selection
-        # view.sel().clear()
-        # for region in target_sel:
-        #     view.sel().add(region)
+        # Update selection
+        # DEV: Attribution to sublime-invert-selection
+        view.sel().clear()
+        for region in target_sel:
+            view.sel().add(region)
 
-        # # Run command
-        # view.run_command('left_delete')
+        # Run command
+        view.run_command('left_delete')
 
         # Load in single.output
         with open(__dir__ + '/example/left_delete/test_files/single.output.py') as f:
-            expected_content = f.read()
+            expected_output = f.read()
 
         # Break up expected selection from content
-        expected_sel = [sublime.Region(6, 6)]
-        expected_content = """def ab():
-    pass
-"""
+        expected_obj = self.__class__.split_sel(expected_output)
+        expected_sel = expected_obj['sel']
+        expected_content = expected_obj['content']
 
         # Assert input to output
         file_region = sublime.Region(0, view.size())
         actual_content = view.substr(file_region)
-        assert expected_content == actual_content
+        print expected_content == actual_content
 
         # Assert current selection to output selection
         actual_sel = view.sel()
-        assert expected_sel[0] == actual_sel[0]
+        print expected_sel[0] == actual_sel[0]
+
+        # Close the view
+
