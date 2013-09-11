e100357b9c714e761c5e9946ed045455ca438832
Todd Wolfson <todd@twolfson.com>
2013-09-11 07:03:20 -0700

    Something in file utils...

--- test.py
+++ test.py
@@ -81,22 +81,22 @@ def main():
     # # Generate scratch plugins directory
     # os.makedirs(scratch_dir)
 
-    # If the scratch plugins directory does not exist, create it
-    if not os.path.exists(scratch_dir):
-        os.makedirs(scratch_dir)
+    # # If the scratch plugins directory does not exist, create it
+    # if not os.path.exists(scratch_dir):
+    #     os.makedirs(scratch_dir)
 
     # TODO: Need to force-reload plugin. Auto-reload is too slow / hard to detect.
     # TODO: Try out command that re-imports a module (the dynamic file) and runs it
     # TODO: The ultimate test is outputting a random number every time at the top level
 
-    # Copy the command.py to call our test
-    # if not os.path.exists(scratch_dir + '/command.py'):
-    if True:
-        shutil.copyfile(__dir__ + '/tmp/command.py', scratch_dir + '/command.py')
+    # # Copy the command.py to call our test
+    # # if not os.path.exists(scratch_dir + '/command.py'):
+    # if True:
+    #     shutil.copyfile(__dir__ + '/tmp/command.py', scratch_dir + '/command.py')
 
-    # Output plugin to directory
-    with open(scratch_dir + '/plugin.py', 'w') as f:
-        f.write(plugin)
+    # # Output plugin to directory
+    # with open(scratch_dir + '/plugin.py', 'w') as f:
+    #     f.write(plugin)
 
     # Start a subprocess to run the plugin
     # TODO: We might want a development mode (runs commands inside local sublime window) and a testing mode (calls out to Vagrant box)
