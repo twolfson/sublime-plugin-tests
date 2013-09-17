6060bb8e3f2f3cdf9c0efd6289990e89d343d0c5
Todd Wolfson <todd@twolfson.com>
2013-09-17 02:15:58 -0700

    As anticipated, `read()` picks up all changes -_-

--- lib/command.py
+++ lib/command.py
@@ -10,7 +10,9 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         # On every run, re-import the test class
         # DEV: If we overwrote command.py, Sublime would refuse to run `tmp_test`
-        sublime_plugin.reload_plugin(__dir__ + '/plugin.py')
-        from plugin import Test
-        test = Test()
-        test.run()
+        # sublime_plugin.reload_plugin(__dir__ + '/plugin.py')
+        with open(__dir__ + '/plugin.py') as f:
+            print f.read()
+        # from plugin import Test
+        # test = Test()
+        # test.run()
