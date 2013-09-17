52b18b89568a6da6d72fe166883b838191111efe
Todd Wolfson <todd@twolfson.com>
2013-09-17 02:24:50 -0700

    You cannot eval a class =(

--- lib/command.py
+++ lib/command.py
@@ -11,7 +11,8 @@ class TmpTestCommand(sublime_plugin.ApplicationCommand):
         # On every run, re-import the test class
         # DEV: If we overwrote command.py, Sublime would refuse to run `tmp_test`
         # sublime_plugin.reload_plugin(__dir__ + '/plugin.py')
-        execfile(__dir__ + '/plugin.py')
+        with open(__dir__ + '/plugin.py') as f:
+            eval(f.read())
         from plugin import Test
         test = Test()
-        test.run()
+        test.run()s
