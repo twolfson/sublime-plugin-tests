0d8b42ca5b62c80c679508e4d6faecdb7ff787e7
Todd Wolfson <todd@twolfson.com>
2013-09-17 23:12:48 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements(filename='requirements.txt'):
+    with open(filename) as f:
+        return f.readlines()
+
+
+setup(
+    name='sublime_plugin_tests',
+    version='0.1.0',
+    description='Testing framework for Sublime plugins',
+    keywords=[
+        'sublime text',
+        'plugin',
+        'test',
+        'framework',
+        'tdd'
+    ],
+    author='Todd Wolfson',
+    author_email='todd@twolfson.com',
+    author_url='http://twolfson.com/',
+    url='https://github.com/twolfson/sublime-plugin-tests',
+    packages=find_packages('lib'),
+    package_dir={'':'lib'},
+    license='UNLICENSE',
+    install_requires=read_requirements(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Text Editors'
+    ]
+)
