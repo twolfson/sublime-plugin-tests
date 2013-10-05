ca6e97dffeca6c5a2bd15b7e2799dcf0d5cdfc13
Todd Wolfson <todd@twolfson.com>
2013-10-05 16:47:39 -0700

    Beginning to iterate on import poc

---
+++ import_scratch/command.py
@@ -0,0 +1,11 @@
+import random
+import sublime_plugin
+
+class SublimeImportDevCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		# Generate and print a random string
+		print 'abc'
+
+		# Write out a new python file that prints out said random string
+
+		# Import and run command
