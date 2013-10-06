c267b90c5f6f9332c5ba8aa6c698f062c0b2babc
Todd Wolfson <todd@twolfson.com>
2013-10-05 21:18:16 -0700

    Almost out of options ;_;

--- test/install.sh
+++ test/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# TODO: See if we can get a plugin to output a simple file
+# TODO: See if we can get a plugin to output a simple file (works in VM, not in Vagrant -- requires ST3 to be started first)
 # TODO: See if it will run in a VM (totally fine)
 # TODO: What happens if we connect an x11-forwarding?
 
