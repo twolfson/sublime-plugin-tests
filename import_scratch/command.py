6925a795c0ef499cf53d28a9e41bd44a79735f5b
Todd Wolfson <todd@twolfson.com>
2013-10-05 17:03:22 -0700

    Exec + compile work great but we need to test out importing too

--- import_scratch/command.py
+++ import_scratch/command.py
@@ -11,17 +11,20 @@ class SublimeImportDevCommand(sublime_plugin.ApplicationCommand):
 		print(num)
 
 		# Write out a new python file that prints out said random number
-		script = """
+		__script = """
 hello = %s
 """ % num
-		f = open(__dir__ + '/test.py', 'w')
-		f.write(script)
-		f.close()
+		__f = open(__dir__ + '/test.py', 'w')
+		__f.write(__script)
+		__f.close()
 
 		# Import and run command
 		# from .test import hello
 		f = open(__dir__ + '/test.py')
-		print(f.read())
+		script = f.read()
+		python_dict = {}
+		exec(compile(script, 'test.py', 'exec'), python_dict, python_dict)
+		print(python_dict['hello'])
 		# import importlib
 		# print(importlib)
 		# print(hello)
