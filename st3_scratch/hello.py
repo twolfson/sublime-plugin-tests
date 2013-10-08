6fdccf382b8ca7b6815588b5f365244bb707351a
Todd Wolfson <todd@twolfson.com>
2013-10-08 01:48:06 -0700

    Successfully running command with a bit of a delay =3

--- st3_scratch/hello.py
+++ st3_scratch/hello.py
@@ -1,11 +1,12 @@
+import sublime
 import sublime_plugin
 
-f = open('/tmp/hey2', 'w')
-f.write('hello')
-f.close()
+def wait_a_sec():
+    sublime.run_command('tmp_test_hey')
+sublime.set_timeout(wait_a_sec, 1000)
 
 class TmpTestHeyCommand(sublime_plugin.ApplicationCommand):
     def run(self):
-        f = open('/tmp/hey', 'w')
+        f = open('/tmp/hey3', 'w')
         f.write('hello')
         f.close()
