3536b2fdd1c4cea69ef97cb42cc82ebce09cc642
Todd Wolfson <todd@twolfson.com>
2013-10-08 01:22:53 -0700

    FUCK YES!!!! I HAVE MADE PROGRESS TO HAVE SUBLIME TEXT INVOKE SOMETHING!!!!

--- st3_scratch/hello.py
+++ st3_scratch/hello.py
@@ -1,5 +1,9 @@
 import sublime_plugin
 
+f = open('/tmp/hey2', 'w')
+f.write('hello')
+f.close()
+
 class TmpTestHeyCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         f = open('/tmp/hey', 'w')
