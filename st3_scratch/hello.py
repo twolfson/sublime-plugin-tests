b0ead0c4607d9dd42f41358764b46627a134e1fa
Todd Wolfson <todd@twolfson.com>
2013-10-08 01:53:50 -0700

    Exploring notes from package_control

--- st3_scratch/hello.py
+++ st3_scratch/hello.py
@@ -1,9 +1,11 @@
 import sublime
 import sublime_plugin
 
+# TODO: This wait loop should look for plugin_host running
 def wait_a_sec():
     sublime.run_command('tmp_test_hey')
 sublime.set_timeout(wait_a_sec, 1000)
+print(sublime.version())
 
 class TmpTestHeyCommand(sublime_plugin.ApplicationCommand):
     def run(self):
