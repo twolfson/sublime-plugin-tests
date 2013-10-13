21eaa04af77d4db9597917a68e0648bf2646d4f3
Todd Wolfson <todd@twolfson.com>
2013-10-12 18:55:37 -0700

    Release 0.2.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sublime_plugin_tests',
-    version='0.2.5',
+    version='0.2.6',
     description='Testing framework for Sublime Text plugins',
     long_description=open('README.txt').read(),
     keywords=[
