b2e73e1b6c09b86ef5bfc344ce081a8e7e2733b3
Todd Wolfson <todd@twolfson.com>
2013-12-25 17:50:15 -0800

    Removing more dev scripts and updating name of test script

--- test/install_pyenv.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install git -y
-git clone git://github.com/yyuu/pyenv.git ~/.pyenv
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
-exec $SHELL
